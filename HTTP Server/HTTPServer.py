e4536f8dac3d80ac829d7f93673e2a54f193dfd6
data-hunter <GabrielLins64>
2021-01-22 06:36:47 -0300

    Added HTTP Server

--- HTTP Server/HTTPServer.py
+++ HTTP Server/HTTPServer.py
@@ -1,3 +1,66 @@
 from socket import *
 import sys
+from signal import signal, SIGINT
 
+def handler(signal_received, frame):
+    global server_socket
+    server_socket.close()
+    print("\nClosing server. Goodbye!")
+    exit(0)
+
+server_port = 12000
+buffer_size = 1024
+signal(SIGINT, handler)
+
+server_socket = socket(AF_INET, SOCK_STREAM)
+server_socket.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+server_socket.bind(('', server_port))
+server_socket.listen(1)
+
+print("##################")
+print("## GabServer =) ##")
+print("##################")
+print("Server has started!")
+print("Server IP:", gethostbyname(gethostname() + ".local"))
+print("Listening on port", server_socket.getsockname()[1])
+print()
+
+while(True):
+    connection_socket, addr = server_socket.accept()
+    connection_socket.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+    print("TCP Connection established with", addr)
+
+    try:
+        msg = connection_socket.recv(buffer_size).decode()
+        requested_file = msg.split()[1]
+        if(requested_file=='/'): requested_file = '/index.html'
+        file = open(requested_file[1:], 'r')
+        data = file.read()
+        file.close()
+
+        HTTP_response = "HTTP/1.1 200 OK\n"
+        HTTP_response += "Content-Type: text/html\n"
+        HTTP_response += "Connection: Closed\r\n"
+        connection_socket.send(HTTP_response.encode())
+        connection_socket.send("\r\n".encode())
+        for line in data:
+            connection_socket.send(line.encode())
+        connection_socket.send("\r\n".encode())
+    
+    except IOError:
+        file = open("notfound.html", "r")
+        data = file.read()
+        file.close()
+        
+        HTTP_response = "HTTP/1.1 404 Not Found\n"
+        HTTP_response += "Content-Type: text/html\n"
+        HTTP_response += "Connection: Closed\r\n"
+        connection_socket.send(HTTP_response.encode())
+        connection_socket.send("\r\n".encode())
+        for line in data:
+            connection_socket.send(line.encode())
+        connection_socket.send("\r\n".encode())
+
+    connection_socket.close()
+    print("TCP Connection closed with", addr)
+        
